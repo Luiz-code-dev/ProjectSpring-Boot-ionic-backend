056bca229f24679b13f66264305d0b2fc30d2648
Luiz Eduardo <programadorlesd@gmail.com>
2021-06-23 22:12:04 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/udemy/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.udemy.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.udemy.cursomc.domain.Produto;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
