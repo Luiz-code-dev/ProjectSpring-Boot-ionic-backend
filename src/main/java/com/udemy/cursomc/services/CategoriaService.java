d3e897f89990676c9e7680b1ba731a2a3449028c
Luiz Eduardo <programadorlesd@gmail.com>
2021-07-02 21:48:12 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/udemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursomc.domain.Categoria;
@@ -44,5 +47,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerpage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerpage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 }
