2aad446badcf3e3185344381898ffb5b3baaacd9
Luiz Eduardo <programadorlesd@gmail.com>
2021-07-03 23:02:59 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/udemy/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/udemy/cursomc/repositories/ClienteRepository.java
@@ -1,12 +1,18 @@
 package com.udemy.cursomc.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.udemy.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 
 }
--- src/main/java/com/udemy/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/udemy/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.udemy.cursomc.domain.Cliente;
 import com.udemy.cursomc.domain.enums.TipoCliente;
 import com.udemy.cursomc.dto.ClienteNewDTO;
+import com.udemy.cursomc.repositories.ClienteRepository;
 import com.udemy.cursomc.resources.exception.FieldMessage;
 import com.udemy.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -30,6 +37,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
