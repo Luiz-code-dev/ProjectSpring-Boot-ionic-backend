6161eb08f58089f6c6514e4b7121181b6808637f
Luiz Eduardo <programadorlesd@gmail.com>
2021-07-18 15:58:50 -0300

    MockEmailService com Logger. Padrões Strategy e Template

--- src/main/java/com/udemy/cursomc/services/DBService.java
+++ src/main/java/com/udemy/cursomc/services/DBService.java
@@ -124,7 +124,7 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Luiz Melo", "luiz.dev.melo@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
