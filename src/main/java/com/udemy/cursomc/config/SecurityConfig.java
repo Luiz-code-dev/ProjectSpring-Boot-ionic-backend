ad9e085f54303c8794d3e51c670e32204bcfb71e
Luiz Eduardo <programadorlesd@gmail.com>
2021-07-31 19:58:06 -0300

    Implementando autorização

--- src/main/java/com/udemy/cursomc/config/SecurityConfig.java
+++ src/main/java/com/udemy/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.udemy.cursomc.security.JWTAuthenticationFilter;
+import com.udemy.cursomc.security.JWTAuthorizationFilter;
 import com.udemy.cursomc.security.JWTUtil;
 
 @Configuration
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
