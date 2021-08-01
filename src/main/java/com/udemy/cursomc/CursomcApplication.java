1e62b9a096e56c1badf361fbbc2db7e7ec2e744a
Luiz Eduardo <programadorlesd@gmail.com>
2021-08-01 19:33:01 -0300

    Salvando arquivos no S3 Amazon

--- src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -1,15 +1,19 @@
 package com.udemy.cursomc;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.udemy.cursomc.services.S3Service;
 
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	
 	public static void main(String[] args) {
@@ -18,7 +22,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\temp\\fotos\\eu.jpeg");
 	}
 
 }
