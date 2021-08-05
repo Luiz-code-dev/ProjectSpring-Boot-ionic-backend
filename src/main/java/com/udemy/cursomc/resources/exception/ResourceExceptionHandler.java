1e31d3cbf6f1e6b1289ee1da432f88886db54cfc
Luiz Eduardo <programadorlesd@gmail.com>
2021-08-04 22:14:13 -0300

    Tratando excecoes adequadamente

--- src/main/java/com/udemy/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/udemy/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.udemy.cursomc.services.exceptions.AuthorizationException;
 import com.udemy.cursomc.services.exceptions.DataIntegrityException;
+import com.udemy.cursomc.services.exceptions.FileException;
 import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -36,14 +40,14 @@ public class ResourceExceptionHandler {
 
 		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
 				System.currentTimeMillis());
-		
+
 		for (FieldError x : e.getBindingResult().getFieldErrors()) {
 			err.addError(x.getField(), x.getDefaultMessage());
 		}
-		
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
-	
+
 	@ExceptionHandler(AuthorizationException.class)
 	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
 
@@ -51,4 +55,43 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
+	// Tratando o FileException
+
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	// Tratando o AmazonServiceException
+
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request) {
+
+		HttpStatus code = HttpStatus.valueOf(e.getErrorCode()); // Pega o code Http que vem da Exceção e transforma em
+																// http status
+		StandardError err = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(code).body(err);
+	}
+
+	// Tratando o AmazonClientException
+
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	//Tratando o AmazonS3Exception
+	
+		@ExceptionHandler(AmazonS3Exception.class)
+		public ResponseEntity<StandardError> amazonS3(AmazonS3Exception e, HttpServletRequest request) {
+
+			StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		}
 }
---
+++ src/main/java/com/udemy/cursomc/services/exceptions/FileException.java
@@ -0,0 +1,19 @@
+package com.udemy.cursomc.services.exceptions;
+
+public class FileException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+
+	
+	public FileException(String msg) {
+		super(msg);
+	}
+	
+	public FileException(String msg, Throwable cause) {
+		super(msg, cause);
+		
+	}
+	
+	
+}
